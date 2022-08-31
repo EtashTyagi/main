acf335419d27aae81c0b3142d0a772f5419998a7
EtashTyagi <etash18@gmail.com>
2022-08-31 22:36:29 +0530

    hosted

---
+++ main_backend/src/main/java/com/et/main/configs/ApiBasePathConfiguration.java
@@ -0,0 +1,13 @@
+package com.et.main.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiBasePathConfiguration implements WebMvcConfigurer {
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api", (clazz)->true);
+    }
+}
