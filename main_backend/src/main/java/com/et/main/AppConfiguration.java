50fdf0911f0a27a94832cf962e655b4c1d0bb984
EtashTyagi <etash18@gmail.com>
2022-08-31 23:52:48 +0530

    commit

--- main_backend/src/main/java/com/et/main/AppConfiguration.java
+++ main_backend/src/main/java/com/et/main/AppConfiguration.java
@@ -1,13 +1,20 @@
-package com.et.main.configs;
+package com.et.main;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ApiBasePathConfiguration implements WebMvcConfigurer {
+public class AppConfiguration implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("/api", (clazz)->true);
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://etashtyagi.tk")
+                .allowCredentials(true).maxAge(3600);
     }
 }
